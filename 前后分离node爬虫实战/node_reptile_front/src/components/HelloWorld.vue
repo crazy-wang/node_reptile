acb1bddff112b45a5a37b7af764398fb5261a37f
wangzq13 <wangzq13@lenovo.com>
2019-04-11 14:04:12 +0800

    修改

--- 前后分离node爬虫实战/node_reptile_front/src/components/HelloWorld.vue
+++ 前后分离node爬虫实战/node_reptile_front/src/components/HelloWorld.vue
@@ -7,7 +7,11 @@
         </el-input>
         <el-button type="success" @click="getPic">点击开始爬取信息照片（存于后台文件夹image）</el-button>
         <el-button type="primary" @click="getInfo">点击开始爬取文本信息（返回到控制台打印展示）</el-button>
-        <el-button type="danger" @click="getHttpInfo">点击开始爬取文本信息（返回到前台页面展示）</el-button>
+        <el-button type="danger" @click="getHttpInfo">点击开始爬取文本信息（非request而是http导致301重定向）</el-button>
+        <div>
+            <el-button type="danger" @click="getJob">点击开始爬取文本信息（返回到前台页面展示）</el-button>
+            <el-button @click="next">下一页</el-button>
+        </div>
     </div>
 </template>
 
@@ -16,7 +20,8 @@
         name: 'HelloWorld',
         data() {
             return {
-                urlValue: 'http://m.juyouqu.com/qu/3187982     '
+                urlValue: 'http://m.juyouqu.com/qu/3187982     ',
+                page: 1
             }
         },
         methods: {
@@ -30,6 +35,18 @@
             async getHttpInfo() {
                 let res = await this.axios.get('http://localhost:3000/info')
                 console.log(res, '嘻嘻哈')
+            },
+            async getJob() {
+                let res = await this.axios.get('http://localhost:3000/job', {
+                    params: {
+                        page: this.page
+                    }
+                })
+                console.log(res, '信息')
+            },
+            next() {
+                this.page+=1
+                this.getJob()
             }
         }
 
@@ -38,7 +55,7 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.hello {
-    text-align: center;
-}
+    .hello {
+        text-align: center;
+    }
 </style>
